74ae0c287301552466afd129a13d073044cb2eb5
Peter Wall <pwall@pwall.net>
2017-02-26 21:01:32 +1100

    Added unmapper and unescape function to URI

--- src/main/java/net/pwall/util/URI.java
+++ src/main/java/net/pwall/util/URI.java
@@ -2,7 +2,7 @@
  * @(#) URI.java
  *
  * javautil Java Utility Library
- * Copyright (c) 2013, 2014 Peter Wall
+ * Copyright (c) 2013, 2014, 2017 Peter Wall
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -32,7 +32,10 @@ package net.pwall.util;
  */
 public class URI {
 
-    private static final CharMapper charMapper = new AbstractCharMapper() {
+    public static final String errorMessage = "URI %xx sequence invalid";
+
+    /** A {@link CharMapper} for use with {@link Strings#escape(String, CharMapper)} etc. */
+    public static final CharMapper charMapper = new AbstractCharMapper() {
         @Override
         public String map(int cp) {
             if (cp > 0x7F) {
@@ -40,20 +43,55 @@ public class URI {
                 ByteArrayBuilder bab = new ByteArrayBuilder();
                 Strings.appendUTF8(bab, cp);
                 for (int i = 0; i < bab.length(); i++)
-                    sb.append(hexMapping(bab.get(i), 2, "%", null));
+                    sb.append(hexMapping(bab.get(i), 2, "%"));
                 return sb.toString();
             }
+            if (cp == ' ')
+                return "+";
             if (!isUnreserved(cp))
                 return hexMapping(cp, 2, "%", null);
             return null;
         }
     };
 
+    /** A {@link CharUnmapper} for use with {@link Strings#unescape(String, CharUnmapper)}
+        etc. */
+    public static final CharUnmapper charUnmapper = new CharUnmapper() {
+        @Override
+        public boolean isEscape(CharSequence s, int offset) {
+            char ch = s.charAt(offset);
+            return ch == '%' || ch == '+';
+        }
+        @Override
+        public int unmap(StringBuilder sb, CharSequence s, int offset) {
+            if (s.charAt(offset) == '+') {
+                sb.append(' ');
+                return 1;
+            }
+            if (offset + 3 <= s.length()) {
+                sb.append((char)(convertHexDigit(s.charAt(offset + 1)) * 16 +
+                        convertHexDigit(s.charAt(offset + 2))));
+                return 3;
+            }
+            throw new IllegalArgumentException(errorMessage);
+        }
+    };
+
+    /**
+     * Private constructor to prevent instantiation.  Attempts to instantiate the class via
+     * reflection will cause an {@link IllegalAccessException}.
+     *
+     * @throws  IllegalAccessException in all cases
+     */
+    private URI() throws IllegalAccessException {
+        throw new IllegalAccessException("Attempt to instantiate URI class");
+    }
+
     /**
      * Test whether a given code point is "unreserved" according to the URI specification
      * <a href="https://tools.ietf.org/html/rfc3986">RFC 3986</a>.
      *
-     * @param cp    the code point to be tested
+     * @param   cp  the code point to be tested
      * @return      {@code true} if the character is unreserved
      */
     public static boolean isUnreserved(int cp) {
@@ -65,11 +103,38 @@ public class URI {
      * Escape a string for use in a URI.  Only subcomponents of a URI (e.g. path elements,
      * parameter names and values etc.) should be encoded, NOT the URI as a whole.
      *
-     * @param s     the string to be escaped
+     * @param   s   the string to be escaped
      * @return      the escaped string
      */
     public static String escape(String s) {
         return Strings.escape(s, charMapper);
+    }
+
+    /**
+     * Unescape a string from a URI.
+     *
+     * @param   s   the string to be converted
+     * @return      the "unescaped" string
+     */
+    public static String unescape(String s) {
+        return Strings.unescape(s, charUnmapper);
+    }
+
+    /**
+     * Convert a hex digit to {@code int}.
+     *
+     * @param   ch  the character containg the hex digit
+     * @return      the {@code int}
+     * @throws      IllegalArgumentException if the character is not a hex digit
+     */
+    protected static int convertHexDigit(char ch) {
+        if (ch >= '0' && ch <= '9')
+            return ch - '0';
+        if (ch >= 'A' && ch <= 'F')
+            return ch - 'A' + 10;
+        if (ch >= 'a' && ch <= 'f')
+            return ch - 'a' + 10;
+        throw new IllegalArgumentException(errorMessage);
     }
 
 }
