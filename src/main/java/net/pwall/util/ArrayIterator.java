8d98475990e1566b3bfc8dd062e4be574bc6855a
Peter Wall <pwall@pwall.net>
2017-04-25 12:26:04 +1000

    Updated comment

--- src/main/java/net/pwall/util/ArrayIterator.java
+++ src/main/java/net/pwall/util/ArrayIterator.java
@@ -2,7 +2,7 @@
  * @(#) ArrayIterator.java
  *
  * javautil Java Utility Library
- * Copyright (c) 2013, 2014 Peter Wall
+ * Copyright (c) 2013, 2014, 2017 Peter Wall
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
